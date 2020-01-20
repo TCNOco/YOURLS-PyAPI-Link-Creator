7b07510ec8473671b374a14731f09a4057e6b4a5
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2020-01-20 14:41:04 +0200

    Init

---
+++ shorten.py
@@ -0,0 +1,54 @@
+import urllib.request
+import json
+import urllib
+from xml.etree import ElementTree as ET
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+from json import dumps
+import clipboard
+
+url = ""
+keyword = ""
+requestURL = ""
+status = ""
+message = ""
+title = ""
+short = ""
+
+
+url = ""
+while url == "":
+    url = input("URL to shorten: ")
+url = url.replace("&", "%26")
+
+keyword = input("Enter a short link name: ")
+if keyword == "":
+    keyword = url.split("/")[-1]
+
+requestURL = "https://tcno.co/s/yourls-api.php" \
+            + "?signature={YourSecretSignature}" \
+            + "&action=shorturl" \
+            + "&keyword=" + keyword \
+            + "&format=json" \
+            + "&url=" + url
+
+root = urllib.request.urlopen(requestURL).read()
+#XML: json = json.loads(dumps(bf.data(fromstring(root))))
+json = json.loads(root)
+print('\n')
+
+try:
+    status = json['status']
+    message = json['message']
+    short = json['shorturl']
+    title = json['title']
+except:
+    print(root)
+
+out = "STATUS:\t\t" + status + "\n" \
+    + "MESSAGE:\t" + message + "\n" \
+    + "TITLE:\t\t" + title + "\n" \
+    + "SHORTURL:\t" + short + "\n"
+print(out)
+
+clipboard.copy(short)
